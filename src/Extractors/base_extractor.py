284ac8e9ca5bc9d47830812b296599a33546f7a8
Lyubomir <lyyubo@gmail.com>
2024-12-25 23:01:05 +0200

    Updated base extractor

---
+++ src/Extractors/base_extractor.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+from pathlib import Path
+from ..utils.logger import app_logger
+from ..utils.performance_monitor import performance_monitor
