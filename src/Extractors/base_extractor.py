1b1b3714294a76dcd5bf32c382f61d10004a0ea1
Lyubomir <lyyubo@gmail.com>
2024-12-26 13:50:38 +0200

    Updated base extractor

--- src/Extractors/base_extractor.py
+++ src/Extractors/base_extractor.py
@@ -3,3 +3,9 @@ import pandas as pd
 from pathlib import Path
 from ..utils.logger import app_logger
 from ..utils.performance_monitor import performance_monitor
+
+class BaseExtractor(ABC):
+    """
+    Базов клас за всички extractors.
+    Дефинира общия интерфейс и функционалност за извличане на данни.
+    """
